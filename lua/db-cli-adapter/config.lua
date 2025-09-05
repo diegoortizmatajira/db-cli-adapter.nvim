159b3788cba07ca6beb4b87549d9e232c3d0adb9
Diego Ortiz <diegoortizmatajira@gmail.com>
2025-09-05 07:22:19 -0400

    Mysql adapter

--- lua/db-cli-adapter/config.lua
+++ lua/db-cli-adapter/config.lua
@@ -5,6 +5,7 @@ local C = {
 		adapters = {
 			psql = require("db-cli-adapter.builtins.psql"),
 			sqlite = require("db-cli-adapter.builtins.sqlite"),
+			mysql = require("db-cli-adapter.builtins.mysql"),
 		},
 		sources = {
 			global = vim.fn.stdpath("data") .. "/db-cli-adapter/global-connections.json",
